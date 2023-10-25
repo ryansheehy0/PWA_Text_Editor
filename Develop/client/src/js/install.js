7c22f80fb184e2d05163076f1cb09ab5bb9282c0
Ryan Sheehy <ryansheehy0@gmail.com>
2023-10-24 19:42:47 -0700

    Removes install button when installed.

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,24 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let appInstalled = localStorage.getItem("appInstalled") || false
 let installEvent
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault()
-  butInstall.style.visibility = "visible"
   installEvent = event
-
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async () => {});
-butInstall.addEventListener("click", () => {
-  installEvent.prompt()
-  butInstall.setAttribute("disabled", true)
+butInstall.addEventListener("click", async () => {
+  await installEvent.prompt()
+  //butInstall.setAttribute("disabled", true)
 })
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   console.log("App Installed")
   butInstall.style.visibility = "hidden"
+  localStorage.setItem("appInstalled", "true")
 });
+
+console.log(appInstalled)
+if(appInstalled){
+  butInstall.style.visibility = "hidden"
+}else{
+  butInstall.style.visibility = "visible"
+}
